1a5fe98e71be132231b214c9cb5991cb2db5ee07
aditii-07 <chdaditisingh@gmail.com>
2026-02-27 15:19:19 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -6,5 +6,18 @@ public class PalindromeCheckerApp {
         System.out.println("     WELCOME TO PALINDROME APP   ");
         System.out.println("=================================");
 
+        // Hardcoded palindrome
+        String word = "madam";
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome ✅");
+        } else {
+            System.out.println(word + " is NOT a Palindrome ❌");
+        }
     }
 }
