04a862b55ea25c511fd00a211ae840601f273836
aditii-07 <chdaditisingh@gmail.com>
2026-02-27 15:21:40 +0530

    UC3: User input palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,23 +1,30 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("=================================");
         System.out.println("     WELCOME TO PALINDROME APP   ");
         System.out.println("=================================");
 
-        // Hardcoded palindrome
-        String word = "madam";
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+
         String reversed = "";
 
         for (int i = word.length() - 1; i >= 0; i--) {
             reversed += word.charAt(i);
         }
 
-        if (word.equals(reversed)) {
+        if (word.equalsIgnoreCase(reversed)) {
             System.out.println(word + " is a Palindrome ✅");
         } else {
             System.out.println(word + " is NOT a Palindrome ❌");
         }
+
+        scanner.close();
     }
 }
